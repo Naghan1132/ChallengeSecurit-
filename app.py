82991f833302e6fc143ccfa3c88a9b378fd21d1a
Nathan Grimault <nagrimault@univ-lyon2.fr>
2024-03-11 20:18:22 +0100

    dad

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+
 # Fonction pour générer un KPI
-def generate_kpi():
-    kpi_value = np.random.randint(100, 1000)
+def generate_kpi(data):
+    kpi_value = data['Status'].count()  # Compter le nombre de requêtes
     return kpi_value
 
 # Fonction pour générer un graphique camembert avec Seaborn
-def generate_pie_chart(background=False):
+def generate_pie_chart(data,ip=None,background=False):
+    deny_count = data[data['Status'] == 'DENY']['Status'].count()
+    permit_count = data[data['Status'] == 'PERMIT']['Status'].count()
     labels = ['Deny', 'Permit']
-    sizes = [np.random.randint(20, 80), np.random.randint(20, 80)]
+    sizes = [deny_count, permit_count]
     colors = ['#C70039', '#7FDF70']
     plt.figure(figsize=(6, 6))
     sns.set(style="whitegrid")
@@ -23,19 +27,17 @@ def generate_pie_chart(background=False):
     return plt
 
 # Fonction pour générer le top 5 des ips sources
-def generate_top_ips():
-    ips = [f"192.168.1.{i}" for i in range(1, 6)]
-    counts = np.random.randint(10, 100, size=5)
-    top_ips_df = pd.DataFrame({'IP Source': ips, 'Nombre de connexions': counts})
-    return top_ips_df
+def generate_top_ips(data):
+    top_ips = data['SRC'].value_counts().head(5).reset_index()
+    top_ips.columns = ['IP Source', 'Nombre de connexions']
+    return top_ips
 
 # Fonction pour générer un graphique de répartition des ports
-def generate_port_distribution(background=False):
-    ports = [f"Port {i}" for i in range(1, 6)]
-    counts = np.random.randint(10, 100, size=5)
-    port_df = pd.DataFrame({'Port': ports, 'Nombre de connexions': counts})
+def generate_port_distribution(data, background=False):
+    port_counts = data['SPT'].value_counts().head(5).reset_index()
+    port_counts.columns = ['Port', 'Nombre de connexions']
     plt.figure(figsize=(10, 6))
-    sns.barplot(x='Port', y='Nombre de connexions', data=port_df, palette='Blues', hue='Port', dodge=False, legend=False)
+    sns.barplot(x='Port', y='Nombre de connexions', data=port_counts, palette='Blues', hue='Port', dodge=False, legend=False)
     plt.xlabel('Ports')
     plt.ylabel('Nombre de connexions')
     plt.title('Répartition des connexions par port')
@@ -43,21 +45,10 @@ def generate_port_distribution(background=False):
         plt.gca().set_facecolor('none')
     return plt
 
-def generate_ml_data():
-    ip_source = [f"192.168.1.{i}" for i in range(1, 11)]
-    ip_destination = [f"10.0.0.{i}" for i in range(1, 11)]
-    ports = [f"Port {i}" for i in range(1, 11)]
-    access = np.random.choice(['Deny', 'Permit'], size=10)
-    rules = [f"Rule {i}" for i in range(1, 11)]
-    clusters = np.random.choice(['Dangereux', 'Inoffensif'], size=10)
-    ml_data_df = pd.DataFrame({'IP Source': ip_source, 'IP Destination': ip_destination,
-                               'Port': ports, 'Accès': access, 'Règle': rules, 'Cluster': clusters})
-    return ml_data_df
-
-def dashboard():
+def dashboard(data):
     st.title("Dashboard")
 
-    # Filtres de date
+    # Filtres de date (si votre dataframe contient une colonne de date)
     start_date = st.sidebar.date_input("Date de début", value=pd.to_datetime('2024-01-01'))
     end_date = st.sidebar.date_input("Date de fin", value=pd.to_datetime('2024-12-31'))
 
@@ -69,50 +60,127 @@ def dashboard():
 
     with col1:
         # Afficher le KPI
-        kpi_value = generate_kpi()
         st.subheader("Nombre de requêtes")
-        st.write(f"La valeur du KPI  : {kpi_value}")
+        kpi_value = generate_kpi(data)
+        st.write(f"La valeur du KPI : {kpi_value}")
 
     with col2:
         # Afficher le graphique camembert
         st.subheader("Répartition DENY/Permit")
-        pie_chart = generate_pie_chart(background=True)
+        pie_chart = generate_pie_chart(data, background=True)
         st.pyplot(pie_chart)
 
     # Afficher le top 5 des IP sources
     st.subheader("Top 5 des IP Sources")
-    top_ips_df = generate_top_ips()
+    top_ips_df = generate_top_ips(data)
     st.table(top_ips_df)
 
     # Afficher la répartition des ports
     st.subheader("Répartition des connexions par port")
-    port_distribution = generate_port_distribution(background=True)
+    port_distribution = generate_port_distribution(data, background=True)
     st.pyplot(port_distribution)
 
-def machine_learning():
+
+
+
+def machine_learning(data):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
     st.title("Machine Learning")
 
     # Générer des données pour l'apprentissage automatique
-    ml_data_df = generate_ml_data()
+    ml_data_df = generate_ml(data)
+    print(ml_data_df)
 
-    # Afficher la grande table avec les données d'apprentissage automatique
-    # Coloration conditionnelle pour toute la ligne
-    def color_row(row):
-        if row['Cluster'] == 'Dangereux':
-            return ['background-color: orange'] * len(row)
-        else:
-            return [''] * len(row)
+    # Afficher un échantillon des données d'apprentissage automatique
+    st.subheader("Échantillon des données d'apprentissage automatique")
+    st.dataframe(ml_data_df.sample(15), height=600)
 
-    styled_ml_data_df = ml_data_df.style.apply(color_row, axis=1)
-    st.dataframe(styled_ml_data_df, height=600)
+    # Créer le nuage de points avec Seaborn
+    plt.figure(figsize=(10, 8))
+    
+    # Sélection des colonnes pertinentes pour le graphique
+    x = ml_data_df['SPT']
+    x = x.head(10000)
+    y = ml_data_df['Rule']
+    y = y.head(10000)
+    clusters = ml_data_df['Cluster']
+    clusters = clusters.head(10000)
+    
+    # Utilisation de Seaborn pour le nuage de points avec coloration en fonction des clusters
+    sns.scatterplot(x=x, y=y, hue=clusters, palette='viridis', alpha=0.5)
+    
+    # Ajout de labels et titre
+    plt.xlabel('SPT')
+    plt.ylabel('Règle')
+    plt.title('Nuage de points avec clusters des individus')
+    
+    plt.legend(title='Cluster')
+    
+    # Affichage du graphique avec Streamlit
+    st.pyplot(plt.gcf())
 
+
+def generate_ml(data): 
+    from sklearn.cluster import KMeans
+    from sklearn.preprocessing import StandardScaler
+
+    # Générer des données pour l'apprentissage automatique
+    ml_data = data.copy()  # Copie des données pour éviter de modifier les données originales
+
+    features = data[['SPT']]
+
+    scaler = StandardScaler()
+    scaled_features = scaler.fit_transform(features)
+    print(scaled_features)
+
+    kmeans = KMeans(n_clusters=2, random_state=42)  # Spécifiez le nombre de clusters souhaité
+    kmeans.fit(scaled_features)
+    
+    cluster_labels = kmeans.labels_
+
+    data['Cluster'] = cluster_labels
+
+    return data
+    
+    
+    # # Séparation des caractéristiques et de la cible
+    # X = ml_data.drop(columns=['Status'])  # Caractéristiques (variables indépendantes)
+    # y = ml_data['Status']  # Cible (variable dépendante)
+
+    # # Diviser les données en ensembles d'entraînement et de test (80% d'entraînement, 20% de test)
+    # from sklearn.model_selection import train_test_split
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+    # # Entraîner un modèle de machine learning (par exemple, une régression logistique)
+    # from sklearn.linear_model import LogisticRegression
+    # model = LogisticRegression()
+    # model.fit(X_train, y_train)
+
+    # # Évaluer la performance du modèle
+    # accuracy = model.score(X_test, y_test)
+    # st.write(f"Précision du modèle : {accuracy}")
+
+    # # Retourner les données utilisées pour l'entraînement et l'évaluation
+    # return {
+    #     'X_train': X_train,
+    #     'X_test': X_test,
+    #     'y_train': y_train,
+    #     'y_test': y_test,
+    #     'model': model
+    # }
+
+    
 def main():
+    data = pd.read_csv("C:/Users/nagrimault/Downloads/final_proto_2.csv")  # Charger vos données à partir du fichier CSV
+
     tabs = ["Dashboard", "Machine Learning"]
     selected_tab = st.sidebar.selectbox("Sélectionnez un onglet", tabs)
     if selected_tab == "Dashboard":
-        dashboard()
+        dashboard(data)
     elif selected_tab == "Machine Learning":
-        machine_learning()
+        machine_learning(data)
 
 if __name__ == "__main__":
     main()
